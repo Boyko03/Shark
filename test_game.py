570945de1c008db47c15f7537b4e1a0134c79415
Boyko03 <boyko100@abv.bg>
2020-03-26 10:56:42 +0200

    Now you have the structure of the tests files.

--- test_game.py
+++ test_game.py
@@ -1,7 +1,17 @@
 import unittest
 from game import Game
 
+class TestCardClass(unittest.TestCase):
+	pass
 
+class TestPersonClass(unittest.TestCase):
+	pass
+
+class TestTeamClass(unittest.TestCase):
+	pass
+
+class TestGameClass(unittest.TestCase):
+	pass
 
 if __name__ == '__main__':
 	unittest.main()
--- test_utls.py
+++ test_utls.py
@@ -1,5 +1,11 @@
 import unittest
 from utls import Utls, Jsonable
 
+class TestUtlsClass(unittest.TestCase):
+	pass
+
+class TestJsonableClass(unittest.TestCase):
+	pass
+
 if __name__ == '__main__':
 	unittest.main()
