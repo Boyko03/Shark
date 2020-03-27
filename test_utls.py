85f6f7cab0dc3d2c585315d1db74af0274d89dc8
Boyko03 <boyko100@abv.bg>
2020-03-27 21:20:09 +0200

    Program now writes result in 'data.jason'

--- test_utls.py
+++ test_utls.py
@@ -1,7 +1,8 @@
 import unittest
 from utls import Utls, Jsonable
-from game import Card
+from game import Game, Team, Card, Person
 from pprint import pprint
+import json
 
 class TestCardClass(unittest.TestCase):
 	pass
@@ -22,7 +23,83 @@ class TestUtlsClass(unittest.TestCase):
 		self.assertNotEqual(cards, shuffled_cards)
 
 class TestJsonableClass(unittest.TestCase):
-	pass
+	def test_add_to_json_with_1_round_only_returns_new_dict(self):
+		game = Game()
+		team1 = Team('team1', (Person('p1'), Person('p2')))
+		team2 = Team('team2', (Person('p3'), Person('p4')))
+		team1.teammates[0].cards = [Card(value, 'C') for value in [val for val in '789TJQKA']]
+		team1.teammates[1].cards = [Card(value, 'D') for value in [val for val in '789TJQKA']]
+		team2.teammates[0].cards = [Card(value, 'H') for value in [val for val in '789TJQKA']]
+		team2.teammates[1].cards = [Card(value, 'S') for value in [val for val in '789TJQKA']]
+		game.team1 = team1
+		game.team2 = team2
+		teams = (team1, team2)
+
+		game.json_history = game.add_to_json()
+		expected = {}
+		expected['game 1:'] = {'round 1:': {}}
+		for i in range(2):
+			expected['game 1:']['round 1:'][teams[i].name] = {}
+			for j in range(2):
+				team = teams[i]
+				p = team.teammates[j]
+				expected['game 1:']['round 1:'][team.name][p.name] = {}
+				expected['game 1:']['round 1:'][team.name][p.name]['cards'] = p.cards
+				expected['game 1:']['round 1:'][team.name][p.name]['announcements'] = p.announcements
+				expected['game 1:']['round 1:'][team.name][p.name]['points'] = p.points
+
+		self.maxDiff = None
+		self.assertEqual(game.json_history, expected)
+
+	def test_add_to_json_with_more_rounds_returns_dict_with_both_new_and_old_data(self):
+		game = Game()
+		team1 = Team('team1', (Person('p1'), Person('p2')))
+		team2 = Team('team2', (Person('p3'), Person('p4')))
+		round_ = 1
+		expected = {}
+
+		for i in range(2):
+			team1.teammates[0].cards = [Card(value, 'C') for value in [val for val in '789TJQKA']]
+			team1.teammates[1].cards = [Card(value, 'D') for value in [val for val in '789TJQKA']]
+			team2.teammates[0].cards = [Card(value, 'H') for value in [val for val in '789TJQKA']]
+			team2.teammates[1].cards = [Card(value, 'S') for value in [val for val in '789TJQKA']]
+			game.team1 = team1
+			game.team2 = team2
+			teams = (team1, team2)
+
+			game.json_history = game.add_to_json()
+			expected['game 1:'] = {f'round {round_}:': {}}
+			for i in range(2):
+				expected['game 1:'][f'round {round_}:'][teams[i].name] = {}
+				for j in range(2):
+					team = teams[i]
+					p = team.teammates[j]
+					expected['game 1:'][f'round {round_}:'][team.name][p.name] = {}
+					expected['game 1:'][f'round {round_}:'][team.name][p.name] = {
+						'cards': p.cards,
+						'announcements': p.announcements,
+						'points': p.points
+					}
+			round_ += 1
+
+	def test_to_json_function_returns_in_json_format(self):
+		game = Game()
+		team1 = Team('team1', (Person('p1'), Person('p2')))
+		team2 = Team('team2', (Person('p3'), Person('p4')))
+		team1.teammates[0].cards = [Card(value, 'C') for value in [val for val in '789TJQKA']]
+		team1.teammates[1].cards = [Card(value, 'D') for value in [val for val in '789TJQKA']]
+		team2.teammates[0].cards = [Card(value, 'H') for value in [val for val in '789TJQKA']]
+		team2.teammates[1].cards = [Card(value, 'S') for value in [val for val in '789TJQKA']]
+		game.team1 = team1
+		game.team2 = team2
+		teams = (team1, team2)
+		game.json_history = game.add_to_json()
+
+		result = game.to_json()
+		expected = json.dumps(game.json_history, indent=4)
+
+		self.assertEqual(result, expected)
+		
 
 if __name__ == '__main__':
 	unittest.main()
--- utls.py
+++ utls.py
@@ -1,5 +1,5 @@
 import random
-from pprint import pprint
+import json
 
 class Card:
 	def __init__(self, value, paint):
@@ -13,12 +13,14 @@ class Card:
 			'H': '♥',
 			'S': '♠'
 		}
-		return f'{self.value} {paints[self.paint]}'
+		return f'{self.value}{paints[self.paint]}'
 
 class Person:
 	def __init__(self, name):
 		self.name = name
 		self.cards = []
+		self.announcements = []
+		self.points = 0
 		#cards = [Card, Card, ...]
 
 class Team:
@@ -45,7 +47,7 @@ class Utls:
 		cards = [Card(val, suit) for val in values for suit in suits]
 
 		random.shuffle(cards)
-		
+
 		return cards
 
 	def draw_cards(self, team1, team2):
@@ -55,5 +57,37 @@ class Utls:
 		pass
 
 class Jsonable:
-	def to_json(self):
-		pass
+	def to_json(self, indent=4):
+		teams = (self.team1, self.team2)
+		for game in range(self.game + 1):
+			for round_ in range(self.round + 1):
+				for team in teams:
+					for p in team.teammates:
+						tmp = self.json_history[f'game {game+1}:'][f'round {round_+1}:'][team.name][p.name]['cards'] = [str(card) for card in p.cards]
+
+		return json.dumps(self.json_history, indent=indent)
+
+
+	def add_to_json(self):
+		game = f'game {self.game + 1}:'
+		g_round = f'round {self.round + 1}:'
+		if self.json_history == {}:
+			self.json_history = {game: {}}
+
+		self.json_history[game][g_round] = {}
+		teams = (self.team1, self.team2)
+
+		for i in range(2):
+			self.json_history[game][g_round][teams[i].name] = {}
+			for j in range(2):
+				self.json_history[game][g_round][teams[i].name][teams[i].teammates[j].name] = {
+					"cards": teams[i].teammates[j].cards,
+					"announcements": teams[i].teammates[j].announcements,
+					"points": teams[i].teammates[j].points
+				}
+
+		return self.json_history
+
+	def write_json(self):
+		with open('data.json', 'a') as f:
+			f.write(self.jsoned)
