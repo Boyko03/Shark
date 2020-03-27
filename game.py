99e57f5fc88d0daeaee1a25a88cc480dab63672d
Boyko03 <boyko100@abv.bg>
2020-03-27 21:19:04 +0200

    Implemented play function in Game class

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ import sys
 from utls import Utls, Card, Person, Team, Jsonable
 
 class Game(Utls, Jsonable):
+	def __init__(self):
+		self.json_history = {}
+		self.round = 0
+		self.game = 0
+
 	def prepare(self):
 		self.team1, self.team2 = self.make_teams()
 		self.cards = shuffle()
@@ -9,9 +14,16 @@ class Game(Utls, Jsonable):
 		self.team1, self.team2 = self.draw_cards()
 
 	def play(self):
-		#Some code
+		while self.team1.wins < 2 and self.team2.wins < 2:
+			while self.team1.points < 150 and self.team2.points < 150 or self.team1.points == self.team2.points:
+				self.json_history = self.add_to_json()
+				self.round += 1
 
-		self.to_json()
+			self.game += 1
+			self.round = 0
+
+		self.jsoned = self.to_json()
+		self.write_json()
 		
 if __name__ == '__main__':
 	main()
