b0a634306aba1f880cad479e0e4a014ad093f7c6
Boyko03 <boyko100@abv.bg>
2020-03-29 19:12:58 +0300

    Now program writes in json after each round.

--- game.py
+++ game.py
@@ -16,14 +16,14 @@ class Game(Utls, Jsonable):
 	def play(self):
 		while self.team1.wins < 2 and self.team2.wins < 2:
 			while self.team1.points < 150 and self.team2.points < 150 or self.team1.points == self.team2.points:
+				
 				self.json_history = self.add_to_json()
+				self.jsoned = self.to_json()
+				self.write_json()
 				self.round += 1
 
 			self.game += 1
 			self.round = 0
-
-			self.jsoned = self.to_json()
-			self.write_json()
 		
 if __name__ == '__main__':
 	main()
